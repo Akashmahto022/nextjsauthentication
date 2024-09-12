9f72291b051b992d73c82c32d4f9f5fc13b7456c
Akashmahto022 <akashmahto1169@gmail.com>
2024-09-12 15:09:04 +0530

    created one api for the signup

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,28 +1,35 @@
+import User from '@/models/userModel';
 import nodemailer from 'nodemailer';
+import bcryptjs from 'bcryptjs'
 
 
 export const sendEmail = async ({ email, emailType, userId }: any) => {
     try {
         // todo configure mail for usage
 
-        const transporter = nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            secure: false, // true for port 465, false for other ports
+        const hashToken = await bcryptjs.hash(userId.toString(), 10)
+
+        if (emailType == "Verify") {
+            await User.findByIdAndUpdate(userId, { verifyToken: hashToken, verifyTokenExpiry: Date.now() + 3600000 })
+        } else if (emailType) { }
+
+        var transport = nodemailer.createTransport({
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525,
             auth: {
-                user: "maddison53@ethereal.email",
-                pass: "jn7jnAPss4f63QBp6D",
-            },
+                user: "8a7dc409cd3f29", // ❌❌
+                pass: "d8428e7887d835" // ❌❌
+            }
         });
 
         const mailOption = {
             from: 'akashmahto@gmail.com', // sender address
             to: email, // list of receivers
             subject: emailType === "Verify" ? "Verify your email" : "Reset your password", // Subject line
-            html: "<b>Hello world?</b>", // html body
+            html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashToken}">here</a> to ${emailType === "Verify" ? "verify your email" : "reset your password"} or copy and past the link below in your browser <br> ${process.env.DOMAIN}/verifyemail?token=${hashToken} </p>`, // html body
         }
 
-        const mailResponse = await transporter.sendMail(mailOption)
+        const mailResponse = await transport.sendMail(mailOption)
 
         return mailResponse
 
