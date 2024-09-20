679380c7462a6b055be668d9694a8c9e2ea9e505
Akashmahto022 <akashmahto1169@gmail.com>
2024-09-20 14:11:34 +0530

    created login api

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,13 +1,47 @@
 import { connect } from '@/dbConfig/dbConfig'
 import User from '@/models/userModel'
+import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 import { NextRequest, NextResponse } from 'next/server'
 
 connect()
 
 export async function POST(request: NextRequest) {
     try {
-        
-    } catch (error) {
-        
+        const reqBody = await request.json()
+
+        const { email, password } = reqBody
+
+        console.log(reqBody)
+
+        const user = await User.findOne({ email })
+        if (!user) {
+            return NextResponse.json({ error: "User not found with this email" }, { status: 400 })
+        }
+
+        console.log(user)
+
+        const validPassword = await bcryptjs.compare(password, user.password)
+
+        if (!validPassword) {
+            return NextResponse.json({ error: "Password is not Correct" }, { status: 500 })
+        }
+
+        const tokenData = {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+
+        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, { expiresIn: '2d' })
+
+        const response = NextResponse.json({ message: "Login Successfully", success: true })
+
+        response.cookies.set("token", token, { httpOnly: true })
+
+        return response
+
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 })
     }
 }
