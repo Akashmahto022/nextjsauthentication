613455b7a874512c15a36073dee7e0d9679f7dbf
Akashmahto022 <akashmahto1169@gmail.com>
2024-09-20 13:55:07 +0530

    created verify email api

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,13 @@
+import { connect } from '@/dbConfig/dbConfig'
+import User from '@/models/userModel'
+import { NextRequest, NextResponse } from 'next/server'
+
+connect()
+
+export async function POST(request: NextRequest) {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
