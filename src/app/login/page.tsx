cba15432243b67dea9e0c60d2c69241a07c2bce7
Akashmahto022 <akashmahto1169@gmail.com>
2024-10-02 14:00:05 +0530

    update login page and get profile route

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 const page = () => {
 
-  const router = useRouter()
+  const router = useRouter() 
   const [user, setUser] = useState({
     email: "",
     password: "",
